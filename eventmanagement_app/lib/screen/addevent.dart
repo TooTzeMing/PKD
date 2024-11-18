77663da782c1914a9659c62e17d63d8d27db2d9a
seanliew6969 <liewkai@graduate.utm.my>
2024-11-19 03:00:07 +0800

    add,edit,delete event

--- eventmanagement_app/lib/screen/addevent.dart
+++ eventmanagement_app/lib/screen/addevent.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddEventScreen extends StatefulWidget {
   const AddEventScreen({Key? key}) : super(key: key);
@@ -14,24 +15,61 @@ class _AddEventScreenState extends State<AddEventScreen> {
   final TextEditingController _descriptionController = TextEditingController();
   final TextEditingController _maxParticipantsController = TextEditingController();
 
-  void _handleSubmit() {
-    // Check if any field is empty
-    if (_nameController.text.isEmpty ||
-        _dateController.text.isEmpty ||
-        _venueController.text.isEmpty ||
-        _descriptionController.text.isEmpty ||
-        _maxParticipantsController.text.isEmpty) {
-      // Show an error message
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Please fill in all fields'),
-          backgroundColor: Colors.red,
-        ),
-      );
-      return;
-    }
+  void _handleSubmit() async {
+  // Check if any field is empty
+  if (_nameController.text.isEmpty ||
+      _dateController.text.isEmpty ||
+      _venueController.text.isEmpty ||
+      _descriptionController.text.isEmpty ||
+      _maxParticipantsController.text.isEmpty) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Please fill in all fields'),
+        backgroundColor: Colors.red,
+      ),
+    );
+    return;
+  }
 
-    // If all fields are filled, proceed with submission (You can put your submit logic here)
+  // Attempt to parse date from input
+  Timestamp eventDate;
+  try {
+    DateTime parsedDate = DateTime.parse(_dateController.text);
+    eventDate = Timestamp.fromDate(parsedDate);
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Invalid date format. Please use YYYYMMDD.'),
+        backgroundColor: Colors.red,
+      ),
+    );
+    return;
+  }
+
+  // Attempt to parse maximum participants
+  int maxParticipants;
+  try {
+    maxParticipants = int.parse(_maxParticipantsController.text);
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Invalid number for maximum participants.'),
+        backgroundColor: Colors.red,
+      ),
+    );
+    return;
+  }
+
+  Map<String, dynamic> eventData = {
+    'name': _nameController.text,
+    'date': eventDate,
+    'venue': _venueController.text,
+    'description': _descriptionController.text,
+    'maxParticipants': maxParticipants,
+  };
+
+  try {
+    await FirebaseFirestore.instance.collection('events').add(eventData);
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -40,15 +78,24 @@ class _AddEventScreenState extends State<AddEventScreen> {
         actions: <Widget>[
           TextButton(
             onPressed: () {
-               Navigator.of(context).pop();
-               Navigator.of(context).pop();
+              Navigator.of(context).pop(); // Close the dialog
+              Navigator.of(context).pop(); // Optionally close the AddEventScreen after success
             },
-            child: Text('Ok')
+            child: Text('OK'),
           ),
         ],
       ),
     );
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Failed to add event: $e'),
+        backgroundColor: Colors.red,
+      ),
+    );
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -120,7 +167,6 @@ class _AddEventScreenState extends State<AddEventScreen> {
 
   @override
   void dispose() {
-    // Dispose controllers when the widget is disposed.
     _nameController.dispose();
     _dateController.dispose();
     _venueController.dispose();
--- eventmanagement_app/lib/screen/eventscreen.dart
+++ eventmanagement_app/lib/screen/eventscreen.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:eventmanagement_app/screen/addevent.dart';
+import 'package:eventmanagement_app/screen/eventdetail.dart';
 import 'package:eventmanagement_app/screen/homescreen.dart';
 import 'package:eventmanagement_app/screen/loginscreen.dart';
+import 'package:intl/intl.dart'; 
+
+
 
 class EventScreen extends StatefulWidget {
   const EventScreen({Key? key}) : super(key: key);
@@ -31,6 +36,11 @@ class EventPageState extends State<EventScreen> {
     }
   }
 
+  String formatDate(Timestamp timestamp) {
+    DateTime dateTime = timestamp.toDate();
+    return DateFormat('yyyy-MM-dd').format(dateTime);  // Formatting date
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,10 +62,45 @@ class EventPageState extends State<EventScreen> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Event Page Content Here'),
+      
+            body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('events').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text("Error fetching data."));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> event = document.data()! as Map<String, dynamic>;
+              // Ensure the 'date' field exists and is a Timestamp
+              String formattedDate = event['date'] != null && event['date'] is Timestamp 
+                                      ? formatDate(event['date'] as Timestamp)
+                                      : 'No date provided';
+              return ListTile(
+                title: Text(event['name'] ?? 'No name provided'),
+                subtitle: Text(event['venue'] ?? 'No venue provided'),
+                trailing: Text(formattedDate),
+                onTap: () {
+  // Obtain the document snapshot
+ // Assuming index is available or use another method to identify the tapped document
+
+  // Navigate to EventDetailScreen with the event ID
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => EventDetailScreen(eventId: document.id), // Pass the event ID
+    ),
+  );
+},
+              );
+            }).toList(),
+          );
+        },
       ),
-      bottomNavigationBar: BottomNavigationBar(
+       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
